d40e904619076f045286f3f4f798a42550f95ede
liangjiemin <liangjiemin@tp-link.com.cn>
2019-06-10 20:54:02 +0800

    19.3

---
+++ demo/src/main/java/com/jimmy/springboot/demo/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.jimmy.springboot.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//19.3
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @RequestMapping("/sayHi")
+    public String sayHi() {
+        return "Hello World!";
+    }
+}
